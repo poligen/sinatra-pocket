5af868ea9bbf698a6049074f3c2d3e506557dfec
Hsuancheng <poligen@gmail.com>
2017-01-24 07:10:31 +0800

    add redcarpet to read markdown

---
+++ data/bookmarks.md
@@ -0,0 +1,23 @@
+            
+
+        - [想要成功可以靠運氣嗎？門都沒有！](http://hsuancheng.net/2016/12/28/peak/)
+
+總算是讀完了。四月的時候，在 freakonomics 上有一期How to Become Great at Just About Anything, 講得非常吸引人是介紹一本書叫《Peak: Secret from new science of expert》，作者是 ANDERS ERICSSON，前陣子簡體版出了，中文取叫做《刻意練習》，拖了很久沒有讀。上周下決心一口氣讀完。覺得收獲非常的多
+
+![](https://scontent-tpe1-1.xx.fbcdn.net/v/t31.0-8/15724663_342123389506895_5588929003407445435_o.jpg?oh=8f72ccd47923f4a5194cde79e0cca44d&oe=58F84D56)
+
+ 
+- [Codeids](http://www.codeids.com/)
+
+Code ideas made of ruby
+
+![]()
+
+ 
+- [H.Spectrum青年翻轉](http://www.ylhspectrum.com/blank-3)
+
+尋找數位時代下的健康領域創業家
+
+![](https://static.wixstatic.com/media/321324_ce55c6a0eb8e4d3eac331bb5e1ed0c28%7Emv2_d_3125_3125_s_4_2.jpg)
+
+ 
--- pocket.rb
+++ pocket.rb
@@ -2,6 +2,7 @@ require 'sinatra'
 require 'sinatra/reloader'
 require 'tilt/erubis'
 require 'link_thumbnailer'
+require 'redcarpet'
 require 'pry'
 
 configure do
@@ -10,7 +11,40 @@ configure do
 end
 
 before do
-  session[:bookmarks] ||= []
+end
+
+def convert_markdown(file_content)
+  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+  markdown.render(file_content)
+end
+
+def bookmark_location
+  File.expand_path('../data/bookmarks.md', __FILE__)
+end
+
+def link_existed?(url)
+  file = File.read(bookmark_location)
+  content = convert_markdown(file)
+  content.include? url
+end
+
+def check_link(link)
+  if link_existed?(link.url.to_s)
+    session[:message] = 'You already add this to your bookmarks'
+    redirect '/'
+  else
+    line = "- [#{link.title}](#{link.url})\n\n#{link.description}\n\n![](#{image_existed?(link)})\n\n "
+    path = bookmark_location
+    File.open(path, 'a+') do |file|
+      file.puts line
+    end
+    session[:message] = 'This link is added to your bookmarks'
+    redirect '/bookmarks'
+  end
+end
+
+def image_existed?(link)
+  link.images.empty? ? '' : link.images.first.src.to_s
 end
 
 get '/' do
@@ -19,16 +53,28 @@ end
 
 post '/' do
   begin
-    link = LinkThumbnailer.generate params[:url]
-    session[:bookmarks] << link
-    redirect '/bookmarks'
-  rescue LinkThumbnailer::Exceptions => e
-    session[:message] = "This link is broken, please check again"
+    link = LinkThumbnailer.generate(params[:url], image_stats: true)
+    check_link(link)
+  rescue LinkThumbnailer::Exceptions
+    session[:message] = 'This link is broken, please check again'
     redirect '/'
   end
 end
 
 get '/bookmarks' do
-  @bookmarks = session[:bookmarks]
+  file = File.read(bookmark_location)
+  @content = convert_markdown(file)
   erb :bookmarks
+end
+
+post '/bookmarks' do
+  File.write(bookmark_location, params[:edit_content])
+  session[:message] = 'Your bookmarks has been updated'
+  redirect '/bookmarks'
+end
+
+# only admin can do it
+get '/bookmarks/edit' do
+  @content = File.read(bookmark_location)
+  erb :edit
 end
